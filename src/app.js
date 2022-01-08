13eda01275d95c658504d2ecee96e303276f5a12
victor <xdjyxu@163.com>
2022-01-08 20:43:18 +0800

    set enviroment

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const Koa = require('koa');
 const koabody = require('koa-body');
+const logger = require('koa-logger');
 
 const router = require('./route');
 
+const NODE_ENV = process.env.NODE_ENV;
+console.log('enviroment:', NODE_ENV);
+
 let app = new Koa();
 app.use(koabody());
+NODE_ENV !== 'test' && app.use(logger());
 app.use(router());
 
 module.exports = app;
